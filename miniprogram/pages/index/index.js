85bc238006e0c1695aff16b4317595f40aad2d06
HL <821890952@qq.com>
2023-04-29 22:09:29 +0800

    feat(notify):公告列表渲染完成

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,17 @@
 Page({
-  async onLoad() {
-    wx.utils.total()
+  data: {
+    notifyDetail: []
+  },
+
+  onLoad() {
+    this.getNotifyDetail()
+
+  },
+  async getNotifyDetail() {
+    const { code, data: notifyDetail } = await wx.http({
+      url: '/announcement'
+    })
+    if (code !== 10000) wx.utils.total()
+    this.setData({ notifyDetail })
   }
 })
